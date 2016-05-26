12ce6c7d8a9ce6e3044e2c9ece783b2ed99e4c76
Rafal Maselek <rafalmaselek@gmail.com>
2016-05-26 11:21:25 +0200

    added basic calculations

--- fun.py
+++ fun.py
@@ -1,18 +1,6 @@
 from math import exp
 
 
-def T(p, m):
-    """
-    :param p: list of momentum vectors of all particles
-    :param m: mass of particles(all have equal one)
-    :return: kinetic energy of system
-    """
-    e = 0.0
-    for k in p:
-        for p_i in k:
-            e += p_i**2/(2*m)
-    return e
-
 
 def V(r, w, m):
     """
@@ -21,34 +9,24 @@ def V(r, w, m):
     :param m:  mass of particles(all have equal one)
     :return: potential energy
     """
-    e = 0.0
-    for x in r:
-        for x_i in x:
-            e += x_i ** 2 / (2 * m)
-    return e
+    E = 0.0
+    for q in r:
+        for x_i in range(0, len(q)):
+            E += q[x_i]**2
+    E *= 0.5 * m * (w**2)
+    return E
 
 
-def H( p, r, m, w):
-    """
-    :param p: list of momentum vectors of all particles
-    :param r: list of position vectors of all particles
-    :param w: omega of oscillator
-    :param m:  mass of particles(all have equal one)
-    :return: total energy (Hamiltonian)
-    """
-    return T(p,m)+V(r,w,m)
-
-
-def exp_fact(k, T, p, r, m, w):
+def exp_fact(k, T, r, m, w):
     """
     :param k: Boltzmann's const
     :param T: temperature
-    :param p: list of momentum vectors of all particles
     :param r: list of position vectors of all particles
     :param w: omega of oscillator
     :param m:  mass of particles(all have equal one)
     :return: exponential factor
     """
-    beta = 1/(k*T)
-    return exp(-beta*H(p, r, m, w))
+    beta = 1.0/(k*T)
+    val = exp(-beta*V(r, m, w))
+    return val
 
--- main.py
+++ main.py
@@ -1,5 +1,91 @@
+from random import *
+from fun import *
+from matplotlib import pyplot as plt
+
+def generate_system(N, x_av, y_av, z_av, sigma):
+    """
+
+    :param N: number of particles
+    :param x_av: for gauss distribution
+    :param y_av: for gauss distribution
+    :param z_av: for gauss distribution
+    :param sigma: for gauss distribution
+    :return: list of lists of particle coordinates:  r=[[x1, y1, z1],...,[xN, yN, zN]]
+    """
+    seed()
+    r = []
+    for particle in range(0,N):
+        xyz = [gauss(x_av, sigma), gauss(y_av, sigma), gauss(z_av, sigma)]
+        r.append(xyz)
+    return r
+
+
+def dynamics(r, steps, sigma0, k_b, T, m, w):
+    """
+
+    :param r: vector of all particles positions
+    :param steps:
+    :param sigma0:
+    :param k_b:
+    :param T:
+    :param m:
+    :param w:
+    :return: potential energy expected value
+    """
+    N = len(r)
+    seed()
+    sigma = sigma0
+    accepted = 0
+    energy = []
+    iter = []
+    for no in range(0, steps):
+        k = randint(0, N-1)  #particle number
+        part = r[k]
+        r_cp = r[:]
+        new_part = []
+        for ii in range(0, 3):
+            #TODO: dodac ograniczenia pudelka
+            new_part.append(gauss(part[ii], sigma))
+        r_cp[k]=new_part
+
+        P_old = exp_fact(k_b, T, r, m, w)
+        energy.append(exp_fact(k_b, T, r, m, w))
+        iter.append(no)
+        P_new = exp_fact(k_b, T, r_cp, m, w)
+        P = P_new/P_old
+        print(P)
+        #print(P)
+        if random() < P:
+            r[k] = new_part
+            accepted += 1
+        if no > 10 and float(accepted)/float(no) < 0.5:
+            sigma -= 1.0/float(no)
+        elif no > 10 and float(accepted)/float(no) > 0.5:
+            sigma += 1.0/float(no)
+        if sigma < 0:
+            sigma=0.1
+    plt.plot(iter, energy)
+    plt.show()
+    print('acceptance: ',float(accepted) / float(steps))
+    E = V(r, w, m)
+    #print('roznica', E_old/E)
+    print('error ', (1 - E/(1.5*N*k_b*T)) * 100, "%")
+
+
+
+
+
+
 def main():
-    print("Hello World!")
+    N = 100
+    m = 1
+    k = 1
+    T = 100
+    w = 2
+    sigma0 = 0.1
+    steps = 10000
+    r = generate_system(N, 10, 10, 10, 1)
+    dynamics(r, steps, sigma0, k, T, m, w)
 
 
 main()
